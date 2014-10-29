942bc0b7048d55826e9bca27d1035a745e78115e
k.kalchenko <k.kalchenko@corp.mail.ru>
2014-10-29 12:25:25 +0300

    app.'s for heroku

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ var path = require('path');
 var app = express();
 var http = require('http');
 
+app.set('port', (process.env.PORT || 3000))
 app.use(express.static(path.join(__dirname, "public"))); // запуск статического файлового сервера, который смотрит на папку public/ (в нашем случае отдает index.html)
 
-app.listen(3000, function(){
-    console.log('Express server');
+app.listen(app.get('port'), function(){
+    console.log('Express server started');
 });
 
 
